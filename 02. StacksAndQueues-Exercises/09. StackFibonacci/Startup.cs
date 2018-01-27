aff31bcac4151729a817e5a23a6fe085c0572bfc
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-01-27 11:39:02 +0200

    Upload Problem 9: Stack Fibonacci.

---
+++ 02. StacksAndQueues-Exercises/09. StackFibonacci/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace _09._StackFibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            Stack<long> stack = new Stack<long>();
+            stack.Push(0);
+            stack.Push(1);
+
+            for (int i = 2; i <= number; i++)
+            {
+                long secondNumber = stack.Pop();
+                long firstNumber = stack.Pop();
+                long thirdNumber = firstNumber + secondNumber;
+                stack.Push(secondNumber);
+                stack.Push(thirdNumber);
+            }
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
