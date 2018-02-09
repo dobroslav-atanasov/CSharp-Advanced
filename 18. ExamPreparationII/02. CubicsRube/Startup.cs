0f512c36d119812632f1a20efaf48f9ac0669cf5
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2018-02-09 10:15:51 +0200

    Upload: Exam Preparation II - Exam 19.06.2016

---
+++ 18. ExamPreparationII/02. CubicsRube/Startup.cs
@@ -0,0 +1,78 @@
+﻿namespace _02._CubicsRube
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[][][] cube = FillCube(number);
+
+            string input = Console.ReadLine();
+            while (input != "Analyze")
+            {
+                int[] numbers = input.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int row = numbers[0];
+                int col = numbers[1];
+                int height = numbers[2];
+                int particle = numbers[3];
+                bool isPointInside = IsPointInside(number, row, col, height);
+                if (isPointInside)
+                {
+                    cube[row][col][height] = particle;
+                }
+                input = Console.ReadLine();
+            }
+
+            long sum = 0;
+            int amountOfCells = (int)Math.Pow(number, 3);
+            int usedCells = 0;
+            for (int row = 0; row < number; row++)
+            {
+                for (int col = 0; col < number; col++)
+                {
+                    for (int height = 0; height < number; height++)
+                    {
+                        if (cube[row][col][height] != 0)
+                        {
+                            sum += cube[row][col][height];
+                            usedCells++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(amountOfCells - usedCells);
+        }
+
+        private static bool IsPointInside(int number, int row, int col, int height)
+        {
+            if (row >= 0 && row < number && col >= 0 && col < number && height >= 0 && height < number)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static int[][][] FillCube(int number)
+        {
+            int[][][] cube = new int[number][][];
+            for (int row = 0; row < number; row++)
+            {
+                cube[row] = new int[number][];
+                for (int col = 0; col < number; col++)
+                {
+                    cube[row][col] = new int[number];
+                    for (int height = 0; height < number; height++)
+                    {
+                        cube[row][col][height] = 0;
+                    }
+                }
+            }
+            return cube;
+        }
+    }
+}
---
+++ 18. ExamPreparationII/04. CubicAssault/Startup.cs
@@ -0,0 +1,71 @@
+﻿namespace _04._CubicAssault
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, Dictionary<string, long>> regions = new Dictionary<string, Dictionary<string, long>>();
+
+            while (input != "Count em all")
+            {
+                string[] inputParts = input.Split(new[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                string regionName = inputParts[0];
+                string meteorType = inputParts[1];
+                long count = long.Parse(inputParts[2]);
+
+                if (!regions.ContainsKey(regionName))
+                {
+                    regions[regionName] = new Dictionary<string, long>();
+                    regions[regionName].Add("Black", 0);
+                    regions[regionName].Add("Red", 0);
+                    regions[regionName].Add("Green", 0);
+                }
+                switch (meteorType)
+                {
+                    case "Black":
+                        regions[regionName][meteorType] += count;
+                        break;
+                    case "Red":
+                        regions[regionName][meteorType] += count;
+                        break;
+                    case "Green":
+                        regions[regionName][meteorType] += count;
+                        break;
+                }
+
+                if (regions[regionName]["Green"] >= 1000000)
+                {
+                    while (regions[regionName]["Green"] >= 1000000)
+                    {
+                        regions[regionName]["Green"] -= 1000000;
+                        regions[regionName]["Red"]++;
+                    }
+                }
+                if (regions[regionName]["Red"] >= 1000000)
+                {
+                    while (regions[regionName]["Red"] >= 1000000)
+                    {
+                        regions[regionName]["Red"] -= 1000000;
+                        regions[regionName]["Black"]++;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, Dictionary<string, long>> region in regions.OrderByDescending(r => r.Value["Black"]).ThenBy(r => r.Key.Length).ThenBy(r => r.Key))
+            {
+                Console.WriteLine(region.Key);
+                foreach (KeyValuePair<string, long> kvp in region.Value.OrderByDescending(m => m.Value).ThenBy(m => m.Key))
+                {
+                    Console.WriteLine($"-> {kvp.Key} : {kvp.Value}");
+                }
+            }
+        }
+    }
+}
